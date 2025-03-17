492efbba572f6d8a12e05f0c022e06bc7ec74995
raddy666 <hamimtahmid66274@gmail.com>
2025-03-18 00:43:31 +0800

    update dependencies: bump expo-router to 4.0.19 and react-native to 0.76.7

--- app/(auth)/signup.tsx
+++ app/(auth)/signup.tsx
@@ -127,7 +127,7 @@ const SignUp: React.FC = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://10.0.2.2:3020/register', {
+      const response = await fetch('http://localhost:3020/register', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- app/index.tsx
+++ app/index.tsx
@@ -75,7 +75,7 @@ const Index: React.FC = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://10.0.2.2:3020/login", {
+      const response = await fetch("http://localhost:3020/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
